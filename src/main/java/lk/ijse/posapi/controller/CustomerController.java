3f2bf987b7c2b630f25c7f11c5d1d9a20b928470
Glen Alloy <glenalloy8@gmail.com>
2024-08-19 10:55:35 +0530

    created util package and created customerController class

---
+++ src/main/java/lk/ijse/posapi/controller/CustomerController.java
@@ -0,0 +1,10 @@
+package lk.ijse.posapi.controller;
+
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/customer")
+public class CustomerController extends HttpServlet {
+
+}
