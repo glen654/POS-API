2ec418847c573eeed7a58ebd09d956cbd2ada2b4
Glen Alloy <glenalloy8@gmail.com>
2024-08-21 11:40:11 +0530

    item save,update,get and delete methods implemented in dao layer

--- src/main/java/lk/ijse/posapi/bo/ItemBOImpl.java
+++ src/main/java/lk/ijse/posapi/bo/ItemBOImpl.java
@@ -1,14 +1,18 @@
 package lk.ijse.posapi.bo;
 
+import lk.ijse.posapi.dao.DAOFactory;
+import lk.ijse.posapi.dao.ItemDAO;
 import lk.ijse.posapi.dto.ItemDTO;
+import lk.ijse.posapi.entity.Item;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class ItemBOImpl implements ItemBO{
+    ItemDAO itemDAO = (ItemDAO) DAOFactory.getDaoFactory().getDao(DAOFactory.DAOTypes.ITEM);
     @Override
     public boolean saveItem(ItemDTO dto, Connection connection) throws SQLException {
-        return false;
+        return itemDAO.save(new Item(dto.getItemCode(), dto.getItemName(), dto.getQtyOnHand(),dto.getUnitPrice()),connection);
     }
 
     @Override
--- src/main/java/lk/ijse/posapi/dao/ItemDAOImpl.java
+++ src/main/java/lk/ijse/posapi/dao/ItemDAOImpl.java
@@ -1,5 +1,6 @@
 package lk.ijse.posapi.dao;
 
+import lk.ijse.posapi.entity.Customer;
 import lk.ijse.posapi.entity.Item;
 
 import java.sql.Connection;
@@ -7,6 +8,9 @@ import java.sql.SQLException;
 
 public class ItemDAOImpl implements ItemDAO{
     static String SAVE_ITEM = "INSERT INTO item (itemCode,itemName,qtyOnHand,unitPrice) VALUES (?,?,?,?)";
+    static String UPDATE_ITEM = "UPDATE item SET itemName=?,qtyOnHand=?,unitPrice=? WHERE itemCode=?";
+    static String GET_ITEM = "SELECT * FROM item WHERE itemCode=?";
+    static String DELETE_ITEM = "DELETE FROM item WHERE itemCode=?";
     @Override
     public boolean save(Item entity, Connection connection) throws SQLException {
         try {
@@ -23,16 +27,45 @@ public class ItemDAOImpl implements ItemDAO{
 
     @Override
     public boolean update(String id, Item entity, Connection connection) {
-        return false;
+        try {
+            var preparedStatement = connection.prepareStatement(UPDATE_ITEM);
+            preparedStatement.setString(1,entity.getItemName());
+            preparedStatement.setInt(2, entity.getQtyOnHand());
+            preparedStatement.setDouble(3, entity.getUnitPrice());
+            preparedStatement.setString(4,id);
+            return preparedStatement.executeUpdate() != 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Item get(String id, Connection connection) throws SQLException {
-        return null;
+        var item = new Item();
+        try {
+            var preparedStatement = connection.prepareStatement(GET_ITEM);
+            preparedStatement.setString(1,id);
+            var resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+                item.setItemCode(resultSet.getString("itemCode"));
+                item.setItemName(resultSet.getString("itemName"));
+                item.setQtyOnHand(resultSet.getInt("qtyOnHand"));
+                item.setUnitPrice(resultSet.getDouble("unitPrice"));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return item;
     }
 
     @Override
     public boolean delete(String id, Connection connection) {
-        return false;
+        try {
+            var preparedStatement = connection.prepareStatement(DELETE_ITEM);
+            preparedStatement.setString(1,id);
+            return preparedStatement.executeUpdate() != 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
