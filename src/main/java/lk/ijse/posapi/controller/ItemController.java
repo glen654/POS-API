f0cb8e1265651103f62b6d8e7a1c81f2de15cdc6
Glen Alloy <glenalloy8@gmail.com>
2024-08-21 11:56:13 +0530

    item post method implemented in item servlet

---
+++ src/main/java/lk/ijse/posapi/controller/ItemController.java
@@ -0,0 +1,83 @@
+package lk.ijse.posapi.controller;
+
+import jakarta.json.JsonException;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.posapi.bo.BOFactory;
+import lk.ijse.posapi.bo.ItemBO;
+import lk.ijse.posapi.dto.CustomerDTO;
+import lk.ijse.posapi.dto.ItemDTO;
+import lk.ijse.posapi.util.UtilProcess;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet(urlPatterns = "/item")
+public class ItemController extends HttpServlet {
+    ItemBO itemBO = (ItemBO) BOFactory.getBOFactory().getBo(BOFactory.BOTypes.ITEM);
+    static Logger logger = LoggerFactory.getLogger(ItemController.class);
+    Connection connection;
+
+    @Override
+    public void init() throws ServletException {
+        logger.info("Initializing Item Controller with call inti method");
+        try {
+            var ctx = new InitialContext();
+            DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/posSystem");
+            this.connection = pool.getConnection();
+        } catch (NamingException | SQLException e) {
+            logger.error("Init failed with", e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //TODO:Save Item
+        if(!req.getContentType().toLowerCase().startsWith("application/json") || req.getContentType() == null){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+        try(var writer = resp.getWriter()) {
+            Jsonb jsonb = JsonbBuilder.create();
+            ItemDTO itemDTO = jsonb.fromJson(req.getReader(),ItemDTO.class);
+            itemDTO.setItemCode(UtilProcess.generateItemId());
+            if(itemBO.saveItem(itemDTO,connection)){
+                writer.write("Item Save Successful");
+                resp.setStatus(HttpServletResponse.SC_CREATED);
+            }else{
+                writer.write("Item Save Unsuccessful");
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            }
+        }catch (JsonException | SQLException e){
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //TODO:Get Item
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //TODO:Update Item
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //TODO:Delete Item
+    }
+}
