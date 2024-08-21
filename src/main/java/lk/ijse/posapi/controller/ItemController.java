02eb130eb8029d1bf523d77d2556302a8d9ce0c9
Glen Alloy <glenalloy8@gmail.com>
2024-08-21 12:59:37 +0530

    item servlet implemented

--- src/main/java/lk/ijse/posapi/controller/ItemController.java
+++ src/main/java/lk/ijse/posapi/controller/ItemController.java
@@ -10,7 +10,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lk.ijse.posapi.bo.BOFactory;
 import lk.ijse.posapi.bo.ItemBO;
-import lk.ijse.posapi.dto.CustomerDTO;
 import lk.ijse.posapi.dto.ItemDTO;
 import lk.ijse.posapi.util.UtilProcess;
 import org.slf4j.Logger;
