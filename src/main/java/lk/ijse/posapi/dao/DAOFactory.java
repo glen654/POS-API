ef2c08bb2c5d0d6013b14990f74f394623f7f8cc
Glen Alloy <glenalloy8@gmail.com>
2024-08-23 12:40:02 +0530

    order and order detials dao layer implemented

--- src/main/java/lk/ijse/posapi/dao/DAOFactory.java
+++ src/main/java/lk/ijse/posapi/dao/DAOFactory.java
@@ -2,6 +2,8 @@ package lk.ijse.posapi.dao;
 
 import lk.ijse.posapi.dao.impl.CustomerDAOImpl;
 import lk.ijse.posapi.dao.impl.ItemDAOImpl;
+import lk.ijse.posapi.dao.impl.OrderDAOImpl;
+import lk.ijse.posapi.dao.impl.OrderDetailDAOImpl;
 
 public class DAOFactory {
     private static DAOFactory daoFactory;
@@ -15,7 +17,7 @@ public class DAOFactory {
     }
 
     public enum DAOTypes{
-        CUSTOMER,ITEM
+        CUSTOMER,ITEM,ORDER,ORDER_DETAIL
     }
 
     public SuperDAO getDao(DAOTypes daoTypes){
@@ -24,6 +26,10 @@ public class DAOFactory {
                 return new CustomerDAOImpl();
             case ITEM:
                 return new ItemDAOImpl();
+            case ORDER:
+                return new OrderDAOImpl();
+            case ORDER_DETAIL:
+                return new OrderDetailDAOImpl();
             default:
                 return null;
         }
