7005ac1529597e36ad381ee5bce1c0b1837e68e6
Glen Alloy <glenalloy8@gmail.com>
2024-08-28 21:32:15 +0530

    added loggs to the transaction

--- src/main/java/lk/ijse/posapi/bo/impl/OrderBOImpl.java
+++ src/main/java/lk/ijse/posapi/bo/impl/OrderBOImpl.java
@@ -3,6 +3,7 @@ package lk.ijse.posapi.bo.impl;
 import lk.ijse.posapi.bo.BOFactory;
 import lk.ijse.posapi.bo.custom.ItemBO;
 import lk.ijse.posapi.bo.custom.OrderBO;
+import lk.ijse.posapi.controller.CustomerController;
 import lk.ijse.posapi.dao.DAOFactory;
 import lk.ijse.posapi.dao.custom.ItemDAO;
 import lk.ijse.posapi.dao.custom.OrderDAO;
@@ -12,6 +13,8 @@ import lk.ijse.posapi.dto.OrderDetailDTO;
 import lk.ijse.posapi.dto.OrderRequestDTO;
 import lk.ijse.posapi.entity.Order;
 import lk.ijse.posapi.entity.OrderDetail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -21,8 +24,10 @@ public class OrderBOImpl implements OrderBO {
     OrderDAO orderDAO = (OrderDAO) DAOFactory.getDaoFactory().getDao(DAOFactory.DAOTypes.ORDER);
     OrderDetailDAO orderDetailDAO = (OrderDetailDAO) DAOFactory.getDaoFactory().getDao(DAOFactory.DAOTypes.ORDER_DETAIL);
     ItemDAO itemDAO = (ItemDAO) DAOFactory.getDaoFactory().getDao(DAOFactory.DAOTypes.ITEM);
+    static Logger logger = LoggerFactory.getLogger(CustomerController.class);
     @Override
     public boolean placeOrder(OrderDTO orderDTO, List<OrderDetailDTO> orderDetailDTOs, Connection connection) throws SQLException {
+        logger.info("Inside of the place order method bo layer");
         try {
             connection.setAutoCommit(false);
 
@@ -31,6 +36,7 @@ public class OrderBOImpl implements OrderBO {
                     new Order(orderDTO.getOrderId(),orderDTO.getOrderDate(),orderDTO.getCustomerId(), orderDTO.getTotalAmount()),
                     connection);
             if (!orderSaved) {
+                logger.error("Data no saved to the order table");
                 connection.rollback();
                 return false;
             }
@@ -41,6 +47,7 @@ public class OrderBOImpl implements OrderBO {
                         new OrderDetail(orderDetail.getOrderId(),orderDetail.getItemCode(),orderDetail.getOrderQty(),orderDetail.getUnitPrice()),
                         connection);
                 if (!orderDetailSaved) {
+                    logger.error("Data not saved to the order details table");
                     connection.rollback();
                     return false;
                 }
@@ -49,6 +56,7 @@ public class OrderBOImpl implements OrderBO {
                 boolean itemQuantityUpdated = itemDAO.updateQuantity(orderDetail.getItemCode(),
                         orderDetail.getOrderQty(),connection);
                 if(!itemQuantityUpdated){
+                    logger.error("Quantity not updated on item table");
                     connection.rollback();
                     return false;
                 }
